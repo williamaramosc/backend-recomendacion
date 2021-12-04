5d3004d2b61dff3ac1b6f599d58f2c975da78f68
Carlos <cortizf@unbosque.edu.co>
2021-12-03 20:34:44 -0500

    Retirado de archivos innecesarios

--- main.py
+++ main.py
@@ -177,10 +177,6 @@ async def usuario_nuevo(lista: ListaRating):
     print(lista1)
     
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"items":{"item_id": item_id, "q": q}}
-
 def recomendacionContenido(paramTitle):
     books = libros_df
 
